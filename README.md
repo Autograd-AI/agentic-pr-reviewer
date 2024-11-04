02d87fedccd0ecc48a81e9971e4a9ad574df36bc
Aaron Joyce <joyceaa@tcd.ie>
2024-11-04 16:55:34 +0000

    Readme update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # appsec
-To run on a local environment, execute `poetry run python -m appsec --to_event <PR_NUMBER>`
+To run on a local environment, execute `poetry run python -m appsec --repo <REPO_FULL_NAME> --to_event <PR_NUMBER>`
--- appsec/cli.py
+++ appsec/cli.py
@@ -12,7 +12,7 @@ def cli():
     parser = argparse.ArgumentParser(prog="appsec", description="AppSec PR requestor")
     parser.add_argument('--repo', help='The GitHub repo to which the commit(s) belong')
     parser.add_argument('--from_event', help='The source commit SHA or tag to compare from')
-    parser.add_argument('--to_event', help='The target commit SHA or tag to compare against')
+    parser.add_argument('--to_event', help='The target commit SHA or tag to compare against', required=True)
 
     args = parser.parse_args()
 
